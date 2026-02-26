9c4f3d5a1de985e672177b2cb40ffd605f3208c0
Ubuntu <samarasinghe@gmail.com>
2026-02-26 09:17:34 +0000

    Cambiar BACKUP_DIR de ~/chopper_backups a ~/config

--- install.sh
+++ install.sh
@@ -3080,7 +3080,7 @@ CONFIGURACION = {
 
 PRESERVAR = {41, 48, 54, 59}  # N.Serie, Dir Modbus, contadores
 
-BACKUP_DIR = os.path.expanduser("~/chopper_backups")
+BACKUP_DIR = os.path.expanduser("~/config")
 
 TODOS_REGISTROS = (
     list(range(71, 85)) +   # Calibracion: 71-84
